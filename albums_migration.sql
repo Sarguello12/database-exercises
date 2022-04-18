eb5d3f8fb69eb423c5703cb13c5d2f9f9d9c345a
Samuel Arguello <sam.rene12@gmail.com>
2022-04-18 14:31:30 -0500

    minor refactor of columns

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,11 +3,11 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE IF NOT EXISTS albums(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-    artist VARCHAR(50) NOT NULL,
+    artist CHAR(100) NOT NULL,
     name VARCHAR(100) NOT NULL,
     release_date INT UNSIGNED,
-    sales FLOAT(9,2),
-    genre VARCHAR(100)
+    sales FLOAT,
+    genre CHAR(200)
 );
 
 DESCRIBE albums;
