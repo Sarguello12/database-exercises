56efce422d37d06b17ec9bfeb48cea990ee6f635
Samuel Arguello <sam.rene12@gmail.com>
2022-04-20 11:00:16 -0500

    completed walkthrough

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -2,7 +2,7 @@ use employees;
 
 SELECT concat(first_name, ' ', last_name) AS 'Employee Name'
 FROM employees
-WHERE last_name LIKE 'E%e';
+WHERE last_name LIKE 'e%e';
 
 SELECT *
 FROM employees
@@ -14,9 +14,9 @@ FROM employees
 WHERE year(hire_date) BETWEEN 1990 AND 1999
 AND month(birth_date) = 12
 AND day(birth_date) = 25
-ORDER BY year(hire_date) DESC;
+ORDER BY birth_date, hire_date DESC;
 
-SELECT concat(first_name, ' ', last_name, ' ', DATEDIFF(curdate(), hire_date)) AS 'Employee time with company'
+SELECT concat(first_name, ' ', last_name, ' ', DATEDIFF(now(), hire_date)) AS 'Employee time with company'
 FROM employees
 WHERE year(hire_date) BETWEEN 1990 AND 1999
   AND month(birth_date) = 12
