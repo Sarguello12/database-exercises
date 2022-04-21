9a92412136d05f426e3a91043614829dd1e012fa
Samuel Arguello <sam.rene12@gmail.com>
2022-04-21 13:50:48 -0500

    completed join_test_db.sql

---
+++ join_test_db.sql
@@ -0,0 +1,49 @@
+CREATE TABLE roles (
+                       id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                       name VARCHAR(100) NOT NULL,
+                       PRIMARY KEY (id)
+);
+
+CREATE TABLE users (
+                       id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                       name VARCHAR(100) NOT NULL,
+                       email VARCHAR(100) NOT NULL,
+                       role_id INT UNSIGNED DEFAULT NULL,
+                       PRIMARY KEY (id),
+                       FOREIGN KEY (role_id) REFERENCES roles (id)
+);
+
+INSERT INTO roles (name) VALUES ('admin');
+INSERT INTO roles (name) VALUES ('author');
+INSERT INTO roles (name) VALUES ('reviewer');
+INSERT INTO roles (name) VALUES ('commenter');
+
+INSERT INTO users (name, email, role_id) VALUES
+                                             ('bob', 'bob@example.com', 1),
+                                             ('joe', 'joe@example.com', 2),
+                                             ('sally', 'sally@example.com', 3),
+                                             ('adam', 'adam@example.com', 3),
+                                             ('jane', 'jane@example.com', null),
+                                             ('mike', 'mike@example.com', null);
+
+INSERT INTO users (name, email, role_id)  VALUES
+                                            ('sam', 'sam@example.com', 2),
+                                            ('jen', 'jen@example.com', 2),
+                                            ('eric', 'eric@example.com', 2),
+                                            ('amy', 'amy@example.com', null);
+
+SELECT users.name AS user_name, roles.name AS role_name
+FROM users
+JOIN roles ON users.role_id = roles.id;
+
+SELECT users.name AS user_name, roles.name AS role_name
+FROM users
+LEFT JOIN roles ON users.role_id = roles.id;
+
+SELECT users.name AS user_name, roles.name AS role_name
+FROM users
+RIGHT JOIN roles ON users.role_id = roles.id;
+
+SELECT roles.name, count(*)
+FROM roles
+JOIN users u ON roles.id = u.role_id GROUP BY roles.name;
